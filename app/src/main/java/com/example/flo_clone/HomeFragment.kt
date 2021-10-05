89a5b13dd62c5f3b6bdf68d555d2dca8eb353675
seongmin-97 <seongmin.tech@gmail.com>
2021-10-05 21:39:12 +0900

    week 1

---
+++ app/src/main/java/com/example/flo_clone/HomeFragment.kt
@@ -0,0 +1,23 @@
+package com.example.flo_clone
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.flo_clone.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment() {
+    lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        binding.homeTitleAlbum1Iv.clipToOutline
+        return binding.root
+    }
+}
